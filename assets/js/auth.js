2ebf728282b517bfed4563efb09187ea4a12edd2
pipersbork <89268966+pipersbork@users.noreply.github.com>
2025-08-02 15:19:01 -0500

    Delete assets directory

--- assets/js/auth.js
+++
@@ -1,9 +0,0 @@
-let GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzxomn7b3HhL_Fjm2gXFxvRvo0cz4CI4ym2ETb2oL37kp1qgxJYzo0hbSmsEv4SYw9Cog/exec";
-
-async function signInWithGoogle() {
-  alert("You will be redirected to grant Google permissions.");
-  window.open(GAS_WEB_APP_URL, "_blank");
-}
-
-document.getElementById("googleSignInBtn").addEventListener("click", signInWithGoogle);
-
