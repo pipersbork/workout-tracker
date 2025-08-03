ad514dbbc1910c7092832951dd280a1dd944cf4f
pipersbork <89268966+pipersbork@users.noreply.github.com>
2025-08-02 19:44:21 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,26 +3,22 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/service-worker.js'
+  '/assets/css/style.css',
+  '/js/main.js',
+  '/js/sync.js',
+  '/js/workout-templates.js'
 ];
 
-// Install
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Activate
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key))))
   );
 });
 
-// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
